258a9643ec8fec3ac25400fb1855f1284c6e4b39
RemagOfficial <52126530+RemagOfficial@users.noreply.github.com>
2022-09-26 02:27:17 +0100

    made replacements work on all occurrences rather than just the first one

--- uwu.js
+++ uwu.js
@@ -2,14 +2,14 @@ function makeUWU() {
     window.document.querySelectorAll('a, p, h1, h2, h3, h4, h5, b, span').forEach((v, k) => {
         v.childNodes.forEach((n, k) => {
             if(n.nodeType == Node.TEXT_NODE)
-            n.textContent = n.textContent.replace(/(r|l)/,"w");
-            n.textContent = n.textContent.replace(/(R|L)/,"W");
-            n.textContent = n.textContent.replace(/(hi|hello)/,"hewwo");
-            n.textContent = n.textContent.replace(/(Hi|Hello)/,"Hewwo");
-            n.textContent = n.textContent.replace(/(father|dad)/,"daddy");
-            n.textContent = n.textContent.replace(/(Father|Dad)/,"Daddy");
-            n.textContent = n.textContent.replace(/(mom|mother)/,"mommy");
-            n.textContent = n.textContent.replace(/(Mom|Mother)/,"Mommy");
+            n.textContent = n.textContent.replace(/(r|l)/g,"w");
+            n.textContent = n.textContent.replace(/(R|L)/g,"W");
+            n.textContent = n.textContent.replace(/(hi|hello)/g,"hewwo");
+            n.textContent = n.textContent.replace(/(Hi|Hello)/g,"Hewwo");
+            n.textContent = n.textContent.replace(/father/g,"daddy");
+            n.textContent = n.textContent.replace(/Father/g,"Daddy");
+            n.textContent = n.textContent.replace(/mother/g,"mommy");
+            n.textContent = n.textContent.replace(/Mother/g,"Mommy");
         });
     });
 }
